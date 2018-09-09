6cc8c4f87b34bbefcb9d613e81f3ade085dfd336
danielferreiradf <danielferreiradk@gmail.com>
2018-09-09 18:58:03 -0300

    added edit item from database

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, startRemoveExpense } from '../actions/expenses';
+import { startEditExpense, startRemoveExpense } from '../actions/expenses';
+import { Link } from 'react-router-dom';
 
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
-    this.props.editExpense(this.props.expense.id, expense);
+    this.props.startEditExpense(this.props.expense.id, expense);
     this.props.history.push('/');
   };
   onRemove = () => {
@@ -20,6 +21,8 @@ export class EditExpensePage extends React.Component {
           onSubmit={this.onSubmit}
         />
         <button onClick={this.onRemove}>Remove</button>
+        <Link to="/">Cancel</Link>
+
       </div>
     );
   }
@@ -30,7 +33,7 @@ const mapStateToProps = (state, props) => ({
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
-  editExpense: (id, expense) => dispatch(editExpense(id, expense)),
+  startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
   startRemoveExpense: (data) => dispatch(startRemoveExpense(data))
 });
 
