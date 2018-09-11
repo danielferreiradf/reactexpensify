24cd7ad4d366e861329358472cc20fc3c4bea848
danielferreiradf <danielferreiradk@gmail.com>
2018-09-11 20:20:04 -0300

    Add Expensify styles

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { startEditExpense, startRemoveExpense } from '../actions/expenses';
-import { Link } from 'react-router-dom';
 
 export class EditExpensePage extends React.Component {
   onSubmit = (expense) => {
@@ -16,13 +15,18 @@ export class EditExpensePage extends React.Component {
   render() {
     return (
       <div>
-        <ExpenseForm
-          expense={this.props.expense}
-          onSubmit={this.onSubmit}
-        />
-        <button onClick={this.onRemove}>Remove</button>
-        <Link to="/">Cancel</Link>
-
+        <div className="page-header">
+          <div className="content-container">
+            <h1 className="page-header__title">Add Expense</h1>
+          </div>
+        </div>
+        <div className="content-container">
+          <ExpenseForm
+            expense={this.props.expense}
+            onSubmit={this.onSubmit}
+          />
+          <button className="button button--secondary" onClick={this.onRemove}>Remove Expense</button>
+        </div>
       </div>
     );
   }
---
+++ src/components/LoadingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoadingPage = () => {
+    return (
+        <div className="loader">
+            <img className="loader__image" src="/images/loading.gif" alt="Loading Page" />
+        </div>
+    )
+}
+
+export default LoadingPage
